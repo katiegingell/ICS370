3ba43c98d9a2f05eba3ce78489dded64f458645c
Katie Gingell <katiegingell@Katies-MacBook-Pro.local>
2023-11-27 22:29:01 -0600

    removed ics370-main folder from last pull request

--- ICS370-main/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.prototype {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.json;
-    requires com.dlsc.formsfx;
-
-    opens com.example.prototype to javafx.fxml;
-    exports com.example.prototype;
-}
